4168d65cb981c193e775d17e97c5724b2e371f11
Fraser Davidson <fraser@frontierhq.com>
2025-02-12 16:57:30 +0000

    WIP

--- py_utils/__init__.py
+++ py_utils/__init__.py
@@ -1,4 +1,5 @@
 from .apply_terraform import apply_terraform
+from .get_azure_short_region import get_azure_short_region
 from .get_env_value import get_env_value
 from .get_version_from_file import get_version_from_file
 from .init_terraform import init_terraform
@@ -6,6 +7,7 @@ from .test_terraform import test_terraform
 
 __all__ = (
     apply_terraform,
+    get_azure_short_region,
     get_env_value,
     get_version_from_file,
     init_terraform,
