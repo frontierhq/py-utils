2f6cdccb0ca5424cc23bf3785f76514b8e091a00
Fraser Davidson <fraser@frontierhq.com>
2025-02-28 17:37:54 +0000

    WIP

--- py_utils/str_to_bool.py
+++ py_utils/str_to_bool.py
@@ -1,15 +1,21 @@
-def str_to_bool(input: str, allow_empty: bool = False) -> bool:
+def str_to_bool(input: str, allow_none: bool = False) -> bool:
     true_values = {"yes", "true", "t", "1", "y"}
     false_values = {"no", "false", "f", "0", "n"}
+
+    if input is None:
+        if allow_none:
+            return False
+
+        raise ValueError("Input cannot be None")
 
     input_lower = input.lower()
 
     if input_lower in true_values:
         return True
-    elif (input_lower in false_values) or allow_empty:
+    elif input_lower in false_values:
         return False
     else:
-        raise ValueError(f"Cannot convert {input_lower} to bool")
+        raise ValueError(f'Cannot convert "{input_lower}" to bool')
 
 
 def _test():
