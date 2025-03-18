e975fb5de4e78c8ea38a6a7cb4c0ed9c090556a9
Fraser Davidson <fraser@frontierhq.com>
2025-03-18 19:11:33 +0000

    WIP

--- py_utils/__init__.py
+++ py_utils/__init__.py
@@ -2,6 +2,7 @@ from .apply_terraform import apply_terraform
 from .get_azure_short_region import get_azure_short_region
 from .get_env_value import get_env_value
 from .get_project_metadata import get_project_metadata
+from .get_terraform_azurerm_backend_config import get_terraform_azurerm_backend_config
 from .get_version_from_file import get_version_from_file
 from .import_terraform_resource import import_terraform_resource
 from .init_terraform import init_terraform
@@ -14,6 +15,7 @@ __all__ = (
     get_azure_short_region,
     get_env_value,
     get_project_metadata,
+    get_terraform_azurerm_backend_config,
     get_version_from_file,
     import_terraform_resource,
     init_terraform,
